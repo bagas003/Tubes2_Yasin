26e82059323cadf9aa00ed651f8567c6b55e1d9b
mutawalle <13521065@std.stei.itb.ac.id>
2023-03-18 13:23:51 +0700

    setup WPF

---
+++ Tubes2_Yasin/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Tubes2_Yasin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string name;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new Person { Name = this.name, Age = 20, PhoneNumber = "123456" };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Create an instance of the OpenFileDialog class
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+
+            // Set the file filter and other properties
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.Multiselect = false;
+
+            // Show the dialog and get the result
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            // Process the result
+            if (result == true)
+            {
+                // Get the selected file name
+                string fileName = openFileDialog.FileName;
+
+                // Read the contents of the file
+                string fileContents = File.ReadAllText(fileName);
+
+                // Do something with the file contents
+                // For example, display the contents in a TextBox
+                this.name = fileContents;
+                Console.WriteLine(fileName);
+                Console.WriteLine(fileContents);
+                DataContext = new Person { Name = this.name, Age = 20, PhoneNumber = "123456" };
+            }
+        }
+    }
+}
---
+++ Tubes2_Yasin/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes2_Yasin
+{
+    public class Person
+    {
+        public string Name { get; set; } = "ewihf";
+        public int Age { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
