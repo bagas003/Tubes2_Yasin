9d541cb75c117cda24d605d68669988671ece6c5
muhhanan <92153837+tarsn@users.noreply.github.com>
2023-03-21 00:24:19 +0700

    up : ui

--- Tubes2_Yasin/obj/Debug/net6.0-windows/App.g.i.cs
+++ Tubes2_Yasin/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Tubes2_Yasin {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Tubes2_Yasin {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public static void Main() {
             Tubes2_Yasin.App app = new Tubes2_Yasin.App();
             app.InitializeComponent();
