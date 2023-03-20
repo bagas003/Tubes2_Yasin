2335136aa84b314833d4b9bc5142d9f0213e42b8
mutawalle <13521065@std.stei.itb.ac.id>
2023-03-21 01:05:35 +0700

    feat: add file validation

--- Tubes2_Yasin/MainWindow.xaml.cs
+++ Tubes2_Yasin/MainWindow.xaml.cs
@@ -18,12 +18,12 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace Tubes2_Yasin
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public string map;
+        public string route { get; set; }
+        public int nodes { get; set; }
+        public int steps { get; set; }
+        public int time { get; set; }
 
         public MainWindow()
         {
@@ -45,6 +45,7 @@ namespace Tubes2_Yasin
             // Process the result
             if (result == true)
             {
+                bool isValid = true;
                 // Get the selected file name
                 string fileName = openFileDialog.FileName;
 
@@ -60,9 +61,33 @@ namespace Tubes2_Yasin
                     for(int j=0;j<length; j++)
                     {
                         matrix[i,j] = lines[i][j];
+                        if (matrix[i,j] != 'T' && matrix[i, j] != 'R' && matrix[i, j] != 'K' && matrix[i, j] != 'X')
+                        {
+                            isValid = false;
+                        }
                     }
                 }
-                createMatrix(matrix);
+                if (isValid)
+                {
+                    createMatrix(matrix);
+                }
+                else
+                {
+                    Grid grid = new Grid();
+
+                    RowDefinition rowDef = new RowDefinition();
+                    grid.RowDefinitions.Add(rowDef);
+
+                    TextBlock textBlock = new TextBlock();
+                    textBlock.Text = "File tidak valid";
+
+                    grid.Children.Add(textBlock);
+
+                    Grid.SetRow(textBlock, 0);
+
+                    Grid mygrid = (Grid)FindName("myMap");
+                    mygrid.Children.Add(grid);
+                }
             }
         }
 
@@ -73,18 +98,22 @@ namespace Tubes2_Yasin
 
             Grid grid = new Grid();
 
-
-            for (int i = 0; i < rows; i++)
+            for(int i = 0; i < rows; i++)
             {
                 RowDefinition rowDef = new RowDefinition();
                 grid.RowDefinitions.Add(rowDef);
+            }
 
+            for (int i = 0; i < rows; i++)
+            {
+                ColumnDefinition colDef = new ColumnDefinition();
+                grid.ColumnDefinitions.Add(colDef);
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
                 for (int j = 0; j < cols; j++)
                 {
-                    ColumnDefinition colDef = new ColumnDefinition();
-                    grid.ColumnDefinitions.Add(colDef);
-
-                    DataContext = new Output { column = rows, row = cols, isi = matrix[0,0] };
 
                     TextBlock textBlock = new TextBlock();
                     if (matrix[i, j] == 'K')
@@ -108,52 +137,46 @@ namespace Tubes2_Yasin
                     myBorder.BorderBrush = Brushes.Black;
                     myBorder.Child = textBlock;
 
-                    textBlock.Width = 600/cols;
-                    textBlock.Height = 600/rows;
-
                     grid.Children.Add(myBorder);
 
                     Grid.SetRow(myBorder, i);
                     Grid.SetColumn(myBorder, j);
-
-                    myBorder.HorizontalAlignment = HorizontalAlignment.Stretch;
-                    myBorder.VerticalAlignment = VerticalAlignment.Stretch;
-
-                    myBorder.Width = 400/cols;
-                    myBorder.Height = 400/rows;
-
                 }
             }
-            grid.Width = 400;
-            grid.Height = 400;
+            grid.Width = 200;
+            grid.Height = 200;
 
             Grid mygrid = (Grid)FindName("myMap");
             mygrid.Children.Add(grid);
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void search(object sender, TextChangedEventArgs e)
         {
+            RadioButton radioTSP = (RadioButton)FindName("radio_TSP");
+            RadioButton radioDFS = (RadioButton)FindName("radio_DFS");
+            RadioButton radioBFS = (RadioButton)FindName("radio_BFS");
+            RadioButton radioBiasa = (RadioButton)FindName("radio_biasa");
 
-        }
+            if (radioTSP.IsChecked == true)
+            {
+                if(radioBFS.IsChecked == true)
+                {
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
+                }else if(radioDFS.IsChecked == true)
+                {
 
-        }
+                }
+            }else if(radioBiasa.IsChecked == true)
+            {
+                if (radioBFS.IsChecked == true)
+                {
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
+                }
+                else if (radioDFS.IsChecked == true)
+                {
 
-        }
-
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
-        {
-
+                }
+            }
         }
     }
 }
