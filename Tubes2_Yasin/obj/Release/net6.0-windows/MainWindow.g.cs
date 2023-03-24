5abbedd79b1158138787f83875dcf7e1be7f72b1
bagas003 <bagasaryo03@gmail.com>
2023-03-24 22:52:16 +0700

    executable

--- Tubes2_Yasin/obj/Release/net6.0-windows/MainWindow.g.cs
+++ Tubes2_Yasin/obj/Release/net6.0-windows/MainWindow.g.cs
@@ -124,7 +124,7 @@ namespace Tubes2_Yasin {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Tubes2_Yasin;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Tubes2_Yasin;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
